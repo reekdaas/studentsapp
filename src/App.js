b5b49048e446972df121c04abd35a31b06b447c1
Rittik Das <dassrittik1998@gmail.com>
2023-10-25 17:09:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import StudentPage from "./Pages/student";
+import { Link, Routes, Route } from "react-router-dom";
+import TeacherPage from "./Pages/teacher";
+import StudentView from "./Components/studentView";
+import StudentAdd from "./Components/studentEdit";
+import ClassView from "./Pages/class";
+import TeacherView from "./Components/teacherView";
+import TeacherEdit from "./Components/teacherEdit";
+import School from "./Pages/school";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <div className="logo">Student Mangament App</div>
+        <ul className="nav">
+          <li>
+            <Link to="/">Students</Link>
+          </li>
+          <li>
+            <Link to="/class">Class</Link>
+          </li>
+          <li>
+            <Link to="/teacher">Teacher</Link>
+          </li>
+          <li>
+            <Link to="/school">School</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<StudentPage />} />
+        <Route path="/teacher" element={<TeacherPage />} />
+        <Route path="/class" element={<ClassView />} />
+        <Route path="/students/:id" element={<StudentView />} />
+        <Route path="/students/edit/:id" element={<StudentAdd />} />
+        <Route path="/addStudent" element={<StudentAdd />} />
+        <Route path="/teacher/:id" element={<TeacherView />} />
+        <Route path="/teacher/edit/:id" element={<TeacherEdit />} />
+        <Route path="/addTeacher" element={<TeacherEdit />} />
+        <Route path="/school" element={<School />} />
+      </Routes>
     </div>
   );
 }
